d25411f5917b6a825459a8d9852916186b6382eb
sandervrielink <sander.vrielink@gmail.com>
2017-04-04 19:31:32 +0000

    Implemented gritter for notifications

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,7 +9,6 @@ class PortfoliosController < ApplicationController
   
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build}
   end
   
   def sort
@@ -36,7 +35,6 @@ class PortfoliosController < ApplicationController
   end
   
   def edit
-    3.times { @portfolio_item.technologies.build}
   end
 
   def update
@@ -71,7 +69,7 @@ class PortfoliosController < ApplicationController
                                       :body,
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:id, :name, :_destroy])
   end
   
   def set_portfolio_item
--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,6 +1,6 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
-  storage :aws
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
